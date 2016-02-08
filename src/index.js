4ca5239f65a7693c002dc9d095d32c751c6b87e9
steelbrain <de.steelbrain@gmail.com>
2016-02-08 18:59:19 +0500

    :new: Add support for non-common file paths

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 'use strict'
 
 const callsite = require('sb-callsite')
-const nameRegex = /[\\\/]packages[\\\/]([^\\\/]+)[\\\/]/
+const regexes = [
+  /[\\\/]packages[\\\/]([^\\\/]+)[\\\/]/,
+  /[\\\/]([\w-_]+)[\\\/](?:lib|src)[\\\/]/,
+  /[\\\/]([\w-_]+)[\\\/][\w-_]+\..+$/
+]
 
 export function guessFromFilePath(filePath) {
-  const matches = nameRegex.exec(filePath)
-  return matches ? matches[1] : null
+  let matches
+  matches = regexes[0].exec(filePath)
+  if (matches) {
+    return matches[1]
+  }
+  matches = regexes[1].exec(filePath)
+  if (matches) {
+    return matches[1]
+  }
+  matches = regexes[2].exec(filePath)
+  if (matches) {
+    return matches[1]
+  }
+  return null
 }
 
 export function guess() {
